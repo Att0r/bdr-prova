ea8d9c94c9852773989221c3ce4d538c17a6ff7c
Gabriel Campos <gabriel.campos@am4.com.br>
2019-06-02 16:30:37 -0300

    Ajustes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,18 +9,24 @@ var structure = {
 var home = {
   init: function () {
     this.slickHeader();
+    this.toggleHeader();
+    this.mascaras();
+    this.scrollTo();
+    this.buttonTop();
   },
 
   slickHeader: function () {
     $('.slick-header').slick({
+      lazyLoad: 'ondemand',
       infinite: true,
       dots: true,
       arrows: false,
-      // autoplay: true,
-      // autoplaySpeed: 5000,
+      autoplay: true,
+      autoplaySpeed: 5000,
     });
 
     $('.slick-section').slick({
+      lazyLoad: 'ondemand',
       infinite: true,
       dots: false,
       arrows: true,
@@ -52,5 +58,29 @@ var home = {
         }
       ]
     });
+
+
   },
+
+  toggleHeader: function () {
+    $('.header-toggle').on('click', function () {
+      $(this).find('i').toggleClass('header-toggle__arrow-down header-toggle__arrow-up');
+      $(this).next('.content-toggle').slideToggle(600);
+      $(this).toggleClass('header-toggle-active', 'slow');
+    });
+  },
+
+  mascaras: function () {
+    $('.phone_with_ddd').mask('(00) 0 0000-0000');
+  },
+
+  scrollTo: function () {
+    $('.scrollTo').click(function () {
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+      }, 500);
+      return false;
+    });
+  },
+
 }
